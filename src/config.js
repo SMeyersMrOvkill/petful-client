8322fd44536392ef761b9098100094cb0fb1dcf4
Samuel L Meyers <thecheesecakejunkie@gmail.com>
2021-01-22 16:21:48 -0600

    Oops, forgot config.js

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
-    //API_ENDPOINT: 'https://enigmatic-island-38831.herokuapp.com'
-    API_ENDPOINT: 'http://localhost:8000'
+    API_ENDPOINT: 'https://enigmatic-island-38831.herokuapp.com'
+    //API_ENDPOINT: 'http://localhost:8000'
 };
