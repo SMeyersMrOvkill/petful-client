93716ef577589711008f227bde28ec3f0c62f451
Samuel L Meyers <thecheesecakejunkie@gmail.com>
2021-01-22 16:50:00 -0600

    Update README.md and Home Page

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -9,6 +9,13 @@ class Home extends React.Component
                 <h1>Petful</h1>
                 <p>Petful is an adoption agency where you can adopt the most recent pet.</p>
                 <Link to="/line">Adopt a pet</Link>
+                <p>To use the app, first click the link above.</p>
+                <p>After you get to the page, it will show you the current state of pet adoption.</p>
+                <p>When you enter your name and click submit, you will be added to the list of people waiting to adopt a pet.</p>
+                <p>The users in front of you will make their decisions about whether they want to adopt the pet or not.</p>
+                <p>This is a tough choice, because you can only adopt the pet that has been in the shelter the longest. So they can take up to 5 seconds to make their decision.</p>
+                <p>When it's your turn, you will see the pets that are available for you to adopt. You can adopt a dog, a cat, or both.</p>
+                <p>When you finally reach the front of the line, people will start lining up behind you.</p>
             </div>
         )
     }
